be538dc1576c61691469ae29b80e0d6cd108a206
Maycon Douglas <maycondss@live.com>
2021-08-24 00:45:07 -0300

    Criação automatica da base de dados + Conexão com MySQL

--- src/main/java/com/demo/study/domain/Categoria.java
+++ src/main/java/com/demo/study/domain/Categoria.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
-@RestController
-@RequestMapping(value = "/categories")
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
