f56a3d5950dbde9584861f3bb3f33dce4a730396
Maycon Douglas <maycondss@live.com>
2021-08-26 16:55:47 -0300

    .

--- src/main/java/com/demo/study/services/CategoriaService.java
+++ src/main/java/com/demo/study/services/CategoriaService.java
@@ -22,10 +22,11 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id) throws ObjectNotFoundException {
         /*O Optional é uma longa discussão sobre o tipo de obj, que pode ser ou não nulo. presente desde o Java 1.8
         para definir se um valor está ou não presente apos a execucao da consulta */
+    public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto n!ao encontrado! Id: " + id + ",  Tipo: " + Categoria.class.getName()));
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
