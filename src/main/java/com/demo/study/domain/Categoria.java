79d134b3590e1b6d5bd708a8583315dbe8d64735
Maycon Douglas <maycondss@live.com>
2021-08-25 23:37:15 -0300

    Criando Produto e adicionando Many to Many com Categoria
    Many to Many ainda precisa de revisão

--- src/main/java/com/demo/study/domain/Categoria.java
+++ src/main/java/com/demo/study/domain/Categoria.java
@@ -1,35 +1,38 @@
 package com.demo.study.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 /*
-* Aqui é declarado como entidade que sera usada em comunicação/geração de query e criação dos objetos
-* para manipulação posterior das classes.
-*
-* */
+ * Aqui é declarado como entidade que sera usada em comunicação/geração de query e criação dos objetos
+ * para manipulação posterior das classes.
+ *
+ * */
 
 @Entity
 public class Categoria implements Serializable {
     /*
-    * implements Serializable define que os dados poderão ser gravados em arquivo (Json por exemplo)
-    * o que facilita no futuro lidar com esses dados em uma API.
-    * */
+     * implements Serializable define que os dados poderão ser gravados em arquivo (Json por exemplo)
+     * o que facilita no futuro lidar com esses dados em uma API.
+     * */
     private static final long serialVersionUID = 1L;
 
     /*
-    * Notação de banco que indica que essa é a chave primaria e é um valor gerado pelo banco.
-    *
-    * */
+     * Notação de banco que indica que essa é a chave primaria e é um valor gerado pelo banco.
+     *
+     * */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    //TODO - Revisar aqui sobre Many to Many
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -37,6 +40,14 @@ public class Categoria implements Serializable {
     public Categoria(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     public Integer getId() {
@@ -57,8 +68,8 @@ public class Categoria implements Serializable {
 
 
     /*Hash code e equals permite verificar se o objeto é identico levando em conta a existencia do hash
-    * de forma a definir se é exatamente o msm obj ou não.
-    * */
+     * de forma a definir se é exatamente o msm obj ou não.
+     * */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
