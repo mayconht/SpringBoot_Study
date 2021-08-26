27791dbfeac694c6ac3affd9b37cc62ceeb4ccb5
Maycon Douglas <maycondss@live.com>
2021-08-25 23:17:07 -0300

    Operação de instanciação de banco. + Comments

--- src/main/java/com/demo/study/StartPointSB.java
+++ src/main/java/com/demo/study/StartPointSB.java
@@ -1,8 +1,13 @@
 package com.demo.study;
 
+import com.demo.study.domain.Categoria;
+import com.demo.study.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
 
 
 /*
@@ -13,6 +18,9 @@ import org.springframework.boot.CommandLineRunner;
 @SpringBootApplication
 public class StartPointSB implements CommandLineRunner {
 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     public static void main(String[] args) {
 
         SpringApplication.run(StartPointSB.class, args);
@@ -20,6 +28,14 @@ public class StartPointSB implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /*
+        * Instanciar dados no banco, basicamente um inicializador de banco.
+        * */
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+        Categoria cat3 = new Categoria(null, "Cozinha");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
         /*
         * String... é o mesmo que String[]
         * Pode receber varios parametros.
--- src/main/java/com/demo/study/services/CategoriaService.java
+++ src/main/java/com/demo/study/services/CategoriaService.java
@@ -9,19 +9,19 @@ import java.util.Optional;
 
 
 /*
-* O Service Alimenta o Resource com as funções necessárias para os endpoints.
-* Basicamente é uma camada de manipulação de dados que serve de ponte entre o Resource e o Repository */
+ * O Service Alimenta o Resource com as funções necessárias para os endpoints.
+ * Basicamente é uma camada de manipulação de dados que serve de ponte entre o Resource e o Repository */
 @Service
 public class CategoriaService {
 
-    /*Autowired injeta as dependencias de repo direto na classe (de forma porca)
-o que retira a necessidade de instanciar a classe (NEW) existem mais coisas
-nesse sentido, mas que não são relevantes Agora.
- */
+    /*Autowired injeta as dependencias de repo direto na classe
+        o que retira a necessidade de instanciar a classe (NEW) existem mais coisas
+        nesse sentido, mas que não são relevantes Agora.
+    */
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+    public Categoria buscar(Integer id) {
         /*O Optional é uma longa discussão sobre o tipo de obj, que pode ser ou não nulo. presente desde o Java 1.8
         para definir se um valor está ou não presente apos a execucao da consulta */
         Optional<Categoria> obj = repo.findById(id);
