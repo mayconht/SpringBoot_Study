4e4a7fc57aa939dc0c1755fee16bf27b3d2be028
Maycon Douglas <maycondss@live.com>
2021-08-28 12:26:35 -0300

    Criando JPA Repository para Cliente

---
+++ src/main/java/com/demo/study/repositories/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.demo.study.repositories;
+
+import com.demo.study.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+/* Camada de acesso de dados
+* Interface que define as implementações do JPA Repository
+* Ele Le a classe Categoria e aplica as interface do JPA
+* */
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
