ce89d49d1a97d59cbd5a8185ab535d019dcad007
Maycon Douglas <maycondss@live.com>
2021-08-28 14:38:22 -0300

    Changing to H2 Database, removing New Classes to validate. (Offline)

--- src/main/java/com/demo/study/repositories/PedidoRepository.java
+++ src/main/java/com/demo/study/repositories/PedidoRepository.java
@@ -1,16 +1,16 @@
-package com.demo.study.repositories;
-
-import com.demo.study.domain.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-/* Camada de acesso de dados
-* Interface que define as implementações do JPA Repository
-* Ele Le a classe Categoria e aplica as interface do JPA
-* */
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-
-}
+//package com.demo.study.repositories;
+//
+//import com.demo.study.domain.Pedido;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//
+///* Camada de acesso de dados
+//* Interface que define as implementações do JPA Repository
+//* Ele Le a classe Categoria e aplica as interface do JPA
+//* */
+//
+//@Repository
+//public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+//
+//}
