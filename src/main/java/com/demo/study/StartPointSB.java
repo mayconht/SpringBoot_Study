01fe60fd1a205a27886e79787ce0ba087c71cd09
Maycon Douglas <maycondss@live.com>
2021-09-05 15:37:46 -0300

    Removendo codigo de dev.

--- src/main/java/com/demo/study/StartPointSB.java
+++ src/main/java/com/demo/study/StartPointSB.java
@@ -42,23 +42,23 @@ public class StartPointSB implements CommandLineRunner {
         /*
         * Instanciar dados no banco, basicamente um inicializador de banco.
         * */
-        Categoria cat1 = new Categoria(null, "Informatica");
-        Categoria cat2 = new Categoria(null, "Escritorio");
-        Categoria cat3 = new Categoria(null, "Cozinha");
-
-        Produto p1 = new Produto(null, "Computador", 2000.00);
-        Produto p2 = new Produto(null, "Papel a4", 20.00);
-        Produto p3 = new Produto(null, "Mouse", 120.00);
-
-        cat1.getProdutos().addAll(Arrays.asList(p1,p3));
-        cat2.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-
-        p1.getCategorias().addAll((Arrays.asList(cat1,cat2)));
-        p2.getCategorias().addAll((Arrays.asList(cat2)));
-        p3.getCategorias().addAll((Arrays.asList(cat1, cat2)));
-
-        categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+//        Categoria cat1 = new Categoria(null, "Informatica");
+//        Categoria cat2 = new Categoria(null, "Escritorio");
+//        Categoria cat3 = new Categoria(null, "Cozinha");
+//
+//        Produto p1 = new Produto(null, "Computador", 2000.00);
+//        Produto p2 = new Produto(null, "Papel a4", 20.00);
+//        Produto p3 = new Produto(null, "Mouse", 120.00);
+//
+//        cat1.getProdutos().addAll(Arrays.asList(p1,p3));
+//        cat2.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+//
+//        p1.getCategorias().addAll((Arrays.asList(cat1,cat2)));
+//        p2.getCategorias().addAll((Arrays.asList(cat2)));
+//        p3.getCategorias().addAll((Arrays.asList(cat1, cat2)));
+//
+//        categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+//        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
     }
 }
