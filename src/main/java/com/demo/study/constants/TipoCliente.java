23262dd49704c2d2ee80d4ca4f04789eef344af4
Maycon Douglas <maycondss@live.com>
2022-03-06 14:51:27 -0300

    100 days of code - day 1: Renivelando JPA

--- src/main/java/com/demo/study/constants/TipoCliente.java
+++
@@ -1,9 +0,0 @@
-package com.demo.study.constants;
-
-public enum TipoCliente {
-    PESSOAFISICA(1),
-    PESSOAJURIDICA(2);
-
-    TipoCliente(int i) {
-    }
-}
--- src/main/java/com/demo/study/resources/exception/ResourceExceptionHandler.java
+++
@@ -1,4 +0,0 @@
-package com.demo.study.resources.exception;
-
-public class ResourceExceptionHandler {
-}
