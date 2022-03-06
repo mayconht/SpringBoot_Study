b801b0bc30a5bef666805a532511b704d2cab65e
Maycon Douglas <maycondss@live.com>
2022-03-06 14:51:49 -0300

    100 days of code - day 1: Renivelando JPA

--- NivelamentoJPA/src/main/java/aplicacao/Programa.java
+++ NivelamentoJPA/src/main/java/aplicacao/Programa.java
@@ -1,2 +1,15 @@
-package aplicacao;public class Programa {
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+    public static void main (String[] args){
+        Pessoa p1 = new Pessoa(1, "Maycon Santos", "maycondss@live.com");
+        Pessoa p2 = new Pessoa(2, "Maycon Santos2", "maycondss2@live.com");
+        Pessoa p3 = new Pessoa(3, "Maycon Santos3", "maycondss3@live.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
 }
--- NivelamentoJPA/src/main/java/dominio/Pessoa.java
+++ NivelamentoJPA/src/main/java/dominio/Pessoa.java
@@ -1,2 +1,52 @@
-package dominio;public class Pessoa {
+package dominio;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String Nome;
+    private String email;
+
+
+    public Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        Nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", Nome='" + Nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
