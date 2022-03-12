65b7f163ece5b0264f2b5fd97084e757dc6c282c
Maycon Douglas <maycondss@live.com>
2022-03-12 15:07:32 -0300

    100 days of code - day 3: Categorias e Classes de Dominio

--- spring/src/main/java/com/estudos/spring/resources/CategoriaResource.java
+++ spring/src/main/java/com/estudos/spring/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.estudos.spring.resources;
 
+import com.estudos.spring.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Classe controladora Rest
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST WORKING";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
+
