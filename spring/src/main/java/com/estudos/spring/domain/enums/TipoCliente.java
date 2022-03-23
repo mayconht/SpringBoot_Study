7fde627a0cfd62885213ef6128dbe195036c0f4e
Maycon Douglas <maycondss@live.com>
2022-03-23 17:06:35 -0300

    100 days of code - day 5: Desenvolvimento Entities + Instanciacao

--- spring/src/main/java/com/estudos/spring/domain/enums/TipoCliente.java
+++ spring/src/main/java/com/estudos/spring/domain/enums/TipoCliente.java
@@ -1,7 +1,37 @@
 package com.estudos.spring.domain.enums;
 
+//Estudar Melhor os Enum Type.
 public enum TipoCliente {
-    PESSOAFISICA,
-    PESSOAJURIDICA;
+    PESSOAFISICA(1, "Pessoa Física"),
+    PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+    private int cod;
+    private String descricao;
+
+    private TipoCliente(int cod, String descricao){
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public int getCod(){
+        return cod;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    //Validação do tipo cliente (proteção de codigo)
+    public static TipoCliente toEnum(Integer cod){
+        if(cod == null){
+            return null;
+        }
+        for(TipoCliente x: TipoCliente.values()){
+            if(cod.equals(x.getCod())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Id Invalido:  " + cod);
+    }
 }
 
--- spring/src/main/java/com/estudos/spring/repositories/ClienteRepository.java
+++ spring/src/main/java/com/estudos/spring/repositories/ClienteRepository.java
@@ -1,10 +1,11 @@
 package com.estudos.spring.repositories;
 
-import com.estudos.spring.domain.Categoria;
+
+import com.estudos.spring.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 //Todas as implementações possiveis serão herdadas da interfaçe, O JPA cuida de do DAO.
 }
