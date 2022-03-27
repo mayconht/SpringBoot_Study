3b5f5aad24913cb24d65277562a55039fa3f6722
Maycon Douglas <maycondss@live.com>
2022-03-27 18:56:34 -0300

    First post

--- spring/src/main/java/com/estudos/spring/services/CategoriaService.java
+++ spring/src/main/java/com/estudos/spring/services/CategoriaService.java
@@ -18,4 +18,9 @@ public class CategoriaService {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Categoria não encontrada: " + id + ", Tipo: " + Categoria.class.getName()));
     }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
