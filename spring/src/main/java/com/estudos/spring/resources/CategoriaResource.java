534cb98998baa2ffb1e9d9b6b196324d0fb629aa
Maycon Douglas <maycondss@live.com>
2022-03-28 09:55:30 -0300

    Delete Categoria /Withou Exception

--- spring/src/main/java/com/estudos/spring/resources/CategoriaResource.java
+++ spring/src/main/java/com/estudos/spring/resources/CategoriaResource.java
@@ -27,7 +27,7 @@ public class CategoriaResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -43,9 +43,18 @@ public class CategoriaResource {
     public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Categoria obj) {
 
         obj.setId(id);
-        obj = service.update(obj);
+        service.update(obj);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+
     }
 }
 
--- spring/src/main/java/com/estudos/spring/services/CategoriaService.java
+++ spring/src/main/java/com/estudos/spring/services/CategoriaService.java
@@ -28,4 +28,10 @@ public class CategoriaService {
         find(obj.getId());
         return repo.save(obj);
     }
+
+    public void delete(Integer id) {
+        find(id);
+        repo.deleteById(id);
+
+    }
 }
